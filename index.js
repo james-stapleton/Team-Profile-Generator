566a052659b720d39397f7a6c5538191611e03d1
“jms” <jstapleton536@gmail.com>
2022-09-11 14:22:28 -0400

    File structure and dependencies

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHTML = require('./utils/generateHTML.js');
+
+generateHTML.log();
+generateHTML.logMessage("Also I can export multiple functions");
---
+++ utils/generateHTML.js
@@ -0,0 +1,5 @@
+
+log = () => console.log("Generate HTML file linked!");
+logMessage = (message) => console.log(message);
+module.exports.log = log;
+module.exports.logMessage = logMessage;

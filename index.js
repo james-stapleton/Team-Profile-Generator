254db068e2ee3dbbce8de3e8efda98112a34b9e6
“jms” <jstapleton536@gmail.com>
2022-09-13 20:57:44 -0400

    update

--- index.js
+++ index.js
@@ -1,163 +1,141 @@
-// I can now create objects of each type and keep going until I decide to stop;
-// Now the question is adding this all to html
-// I could add another function in each object creation instance that then appends the object to an html string literal
-// for example: <Header> <h1> Your Team: </h1> </Header>    <div id = manager> ${manager} </div> 
-// Each time I create a new object, I append that string template with the object variable into my html 
-
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateBaseHTML = require('./utils/generateHTML.js');
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateBaseHTML = require("./utils/generateHTML.js");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const teamArray = [];
 
 // array of questions for prompt to create new Manager instance object
 const questionsManager = [
-{
+  {
     type: "input",
     name: "name",
-    message: "What is the manager's name?"
-},
-{
+    message: "What is the manager's name?",
+  },
+  {
     type: "input",
     name: "email",
     message: "Email? ",
-},
-{
+  },
+  {
     type: "input",
     name: "ID",
-    message: "ID? "
-},
-{
+    message: "ID? ",
+  },
+  {
     type: "input",
     name: "office",
-    message: "office? "
-}
+    message: "office? ",
+  },
 ];
 // Array of questions for new engineer
 const questionsEngineer = [
-{
+  {
     type: "input",
     name: "name",
     message: "What is the engineer's name?",
-},
-{
+  },
+  {
     type: "input",
     name: "ID",
-    message: "ID? "
-},
-{
+    message: "ID? ",
+  },
+  {
     type: "input",
     name: "email",
-    message: "Email? "
-},
-{
+    message: "Email? ",
+  },
+  {
     type: "input",
     name: "github",
-    message: "Github username? "
-}
-]
+    message: "Github username? ",
+  },
+];
 // array of questions for new intern
 const questionsIntern = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is the intern's name?",
-    },
-    {
-        type: "input",
-        name: "ID",
-        message: "ID? "
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Email? "
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "School? "
-    }
-    ]
+  {
+    type: "input",
+    name: "name",
+    message: "What is the intern's name?",
+  },
+  {
+    type: "input",
+    name: "ID",
+    message: "ID? ",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Email? ",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "School? ",
+  },
+];
 // function to create menu to select which type of team member you'd like to add, or to finish the program
 function createMenu() {
-// prompt the user with a list, right now only containing 3 options in a switch statement, but easily scalable if new employee types are added in the future
-// Each switch case includes a call to a function that user an inquirer prompt to create a new object of the desired type
-    inquirer
+  // prompt the user with a list, right now only containing 3 options in a switch statement, but easily scalable if new employee types are added in the future
+  // Each switch case includes a call to a function that uses an inquirer prompt to create a new object of the desired type
+  inquirer
     .prompt([
-        {
-            type: "list",
-            name: "employee",
-            message: "Add an engineer or an intern",
-            choices: ['Engineer', 'Intern', 'Finish'] 
-        }
+      {
+        type: "list",
+        name: "employee",
+        message: "Add an engineer or an intern",
+        choices: ["Engineer", "Intern", "Finish"],
+      },
     ])
     .then(({ employee }) => {
-        switch(employee) {
-            case "Engineer":
-                engineerPrompt();
-                break;
-            case "Intern":
-                internPrompt();
-                break;
-            default:
-                console.log(teamArray);
-                console.log(generateBaseHTML(teamArray));
-                writeHTML(generateBaseHTML(teamArray));
-        }
-    }
-    
-    )
+      switch (employee) {
+        case "Engineer":
+          engineerPrompt();
+          break;
+        case "Intern":
+          internPrompt();
+          break;
+        default:
+          console.log(teamArray);
+          console.log(generateBaseHTML(teamArray));
+          writeHTML(generateBaseHTML(teamArray));
+      }
+    })
     .catch((err) => console.error(err));
 }
 
 // start by creating a manager
 function init() {
-inquirer
-    .prompt(questionsManager)
-    .then((data) => {
-        const manager = new Manager(data.name, data.ID, data.email, data.office);
-        teamArray.push(manager);
-        console.log (manager);
-        createMenu();
-    })
+  inquirer.prompt(questionsManager).then((data) => {
+    const manager = new Manager(data.name, data.ID, data.email, data.office);
+    teamArray.push(manager);
+    console.log(manager);
+    createMenu();
+  });
 }
 
-
-
-// function to create a new engineer object using the questionsEngineer array 
+// function to create a new engineer object using the questionsEngineer array
 function engineerPrompt() {
-    inquirer
-    .prompt(questionsEngineer)
-    .then((data) => {
-        let engineer = new Engineer(data.name, data.ID, data.email, data.github);
-        teamArray.push(engineer);
-        createMenu();
-    })
+  inquirer.prompt(questionsEngineer).then((data) => {
+    let engineer = new Engineer(data.name, data.ID, data.email, data.github);
+    teamArray.push(engineer);
+    createMenu();
+  });
 }
-
-// add .catch
-// .finally run regardless of error
-// throw keyword (err)
 
 function internPrompt() {
-    inquirer
-    .prompt(questionsIntern)
-    .then((data) => {
-        console.log(data);
-        let intern = new Intern(data.name, data.ID, data.email, data.school);
-        teamArray.push(intern);
-        createMenu();
-    })
-    
+  inquirer.prompt(questionsIntern).then((data) => {
+    console.log(data);
+    let intern = new Intern(data.name, data.ID, data.email, data.school);
+    teamArray.push(intern);
+    createMenu();
+  });
 }
 
-
 function writeHTML(stringHTML) {
-    let str = stringHTML.toString();
-    let template = `<!DOCTYPE html>
+  let str = stringHTML.toString();
+  let template = `<!DOCTYPE html>
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
@@ -184,19 +162,15 @@ function writeHTML(stringHTML) {
         <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
       </body>
     </html>
-`
-    
-    // .then and .catch because it now returns promise
-    fs.writeFile("./template.html", template, (err) => {
+`;
+
+  fs.writeFile("./template.html", template, (err) => {
     if (err) {
-        console.log(err);
+      console.log(err);
+    } else {
+      console.log("success");
     }
-    else {
-        console.log("success");
-    }
-})
+  });
 }
-
-
 
 init();
